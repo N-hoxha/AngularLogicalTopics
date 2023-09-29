2d4f00bdf9c25559ddaad4fd4321dc21cbc2065e
N-hoxha <hoxhaneritan2020@gmail.com>
2023-09-29 11:18:09 +0200

    ,,,,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularLogicalTopics';
+
+  loginText = 'Login';
+  signUpText = 'Sign Up'; 
+  lessons = ['Lesson 1', 'Lessons 2'];
+
+  totalEstimate = 10;
+  ctx = {estimate: this.totalEstimate};
+
+  
+  login() {console.log('Login');  }
+
+  signUp() {  console.log('Sign Up'); }
+
 }
--- src/app/pages/ng-content-container-outlet/ng-content-container-outlet.component.ts
+++ src/app/pages/ng-content-container-outlet/ng-content-container-outlet.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NgContentContainerOutletComponent {
 
+  
   studentList: any[] = [
     {
       id: 1,
